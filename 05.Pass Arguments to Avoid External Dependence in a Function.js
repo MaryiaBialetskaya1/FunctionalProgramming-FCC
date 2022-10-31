325b479b318e844b4b2c09c196857da335413726
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-30 21:34:48 -0400

    add solution

---
+++ 05.Pass Arguments to Avoid External Dependence in a Function.js
@@ -0,0 +1,13 @@
+//TASK: Let's update the incrementer function to clearly declare its dependencies.
+
+//Write the incrementer function so it takes an argument, and then returns a result after increasing the value by one.
+
+// The global variable
+let fixedValue = 4;
+
+function incrementer(value) {
+  return value + 1;
+}
+let newValue = incrementer(fixedValue);
+console.log(fixedValue)
+console.log(newValue)
