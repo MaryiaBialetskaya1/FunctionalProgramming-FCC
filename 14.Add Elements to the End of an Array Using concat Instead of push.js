25e2af2b0b24d38bbb2f98c9843b3631d95defeb
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-01 14:39:19 -0400

    add solution

---
+++ 14.Add Elements to the End of an Array Using concat Instead of push.js
@@ -0,0 +1,11 @@
+//TASK:Change the nonMutatingPush function so it uses concat to merge newItem to the end of original without mutating original or newItem arrays. The function should return an array.
+function nonMutatingPush(original, newItem) {
+    return original.concat(newItem);
+  }
+
+  const first = [1, 2, 3];
+  const second = [4, 5];
+  nonMutatingPush(first, second);
+  console.log(first)
+  console.log(second)
+  console.log(nonMutatingPush(first, second))
